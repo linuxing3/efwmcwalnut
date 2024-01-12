f2893ceba360e271fbcf735e65cad5facf7ebcff
linuxing3 <linuxing3@qq.com>
2024-01-12 10:48:51 +0800

    [init]

---
+++ app/xmake.lua
@@ -0,0 +1,20 @@
+--- @diagnostic disable:undefined-global
+
+add_rules('mode.debug', 'mode.release')
+
+target('wgpuapp')
+set_languages('c++20')
+set_kind('binary')
+add_files('**.cpp')
+add_includedirs('.')
+add_defines('RESOURCE_DIR="./wgpu"')
+add_defines('WEBGPU_BACKEND_WGPU')
+add_packages('glfw3webgpu', 'glfw-walnut', 'imgui-walnut', 'vulkan-headers', 'stb', 'tinyobjloader')
+add_packages('Efwmc')
+add_includedirs('$(projectdir)/vendor/webgpu/include')
+add_includedirs('$(projectdir)/vendor/webgpu/include/webgpu')
+add_linkdirs('$(projectdir)/vendor/webgpu/bin/linux-x86_64')
+add_links('wgpu')
+after_build(function(target)
+    os.cp('$(scriptdir)/resources/shaders/wgpu', target:targetdir())
+end)
---
+++ xmake.lua
@@ -0,0 +1,18 @@
+--- @diagnostic disable:undefined-global
+
+set_project('walnut-lib')
+
+set_version('0.0.1')
+
+set_xmakever('2.7.9')
+
+set_warnings('all')
+set_languages('c++20')
+
+add_repositories('xrepos xmake/repos')
+
+set_allowedplats('windows', 'linux', 'macosx')
+
+includes('xmake')
+includes('Source')
+includes('app')
